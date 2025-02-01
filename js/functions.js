935af76b990cd076ff0d91d15d0f997d4f2fc41a
Peso4ek-program <timur.sibgatov023@mail.ru>
2025-02-01 18:33:59 +0500

    Добавляет готовые функции

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+function checkingLenght(str,maxLength){
+  return str.length <= maxLength;
+}
+
+checkingLenght('строка проверки', 15);
+
+function searchPalindrome(row){
+  const newRow = row.replaceAll('','').toLowerCase();
+  let reversedRow = '';
+  for (let i = newRow.length - 1;i >= 0; i--){
+    reversedRow += newRow[i];
+  }
+  return newRow === reversedRow;
+}
+
+searchPalindrome('доввод');
+
+function searchNumber(row){
+  const inputRow = row.toString();
+  let result = '';
+  for (let i = 0; i < inputRow.length; i++){
+    const char = inputRow[i];
+    const num = parseInt(char,10);
+    if(!Number.isNaN(num)){
+      result += char;
+    }
+  }
+  if (result === ''){
+    return NaN;
+  }
+  return parseInt(result,10);
+}
+
+console.log(searchNumber('Всем привет2025'));
